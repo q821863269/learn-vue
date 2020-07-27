04c70b96ea040d78da5228086fddf1660998d8c7
LinKun <821863269@qq.com>
2020-07-27 18:08:25 +0800

    learning

--- 20-项目综合练习/supermall/src/views/detail/Detail.vue
+++ 20-项目综合练习/supermall/src/views/detail/Detail.vue
@@ -1,36 +1,60 @@
 <template>
   <div id="detail">
     <detail-nav-bar />
+    <detail-swiper :top-images="topImages" />
+    <detail-base-info :goods="goods" />
   </div>
 </template>
 <script>
 import DetailNavBar from "./childComps/DetailNavBar";
+import DetailSwiper from "./childComps/DetailSwiper";
+import DetailBaseInfo from "./childComps/DetailBaseInfo";
 
-import detailApi from "network/detail";
+import {
+  getDetail,
+  getRecommend,
+  Goods,
+  Shop,
+  GoodsParam,
+} from "network/detail";
 
 export default {
   name: "Detail",
   components: {
-    DetailNavBar
+    DetailNavBar,
+    DetailSwiper,
+    DetailBaseInfo,
   },
   data() {
     return {
       iid: null,
-      result: {}
+      topImages: [],
+      goods: {},
     };
   },
   methods: {
     getGoodsDetail() {
-      detailApi.getGoodsDetail(this.iid).then(res => {
-        this.result = res.result;
-        console.log(this.result);
+      getDetail(this.iid).then((res) => {
+        // 2.1.获取结果
+        const data = res.result;
+        console.log(data);
+
+        // 2.2.获取顶部信息
+        this.topImages = data.itemInfo.topImages;
+
+        // 2.3.获取商品信息
+        this.goods = new Goods(
+          data.itemInfo,
+          data.columns,
+          data.shopInfo.services
+        );
       });
-    }
+    },
   },
   created() {
     this.iid = this.$route.params.iid;
     this.getGoodsDetail();
-  }
+  },
 };
 </script>
 
